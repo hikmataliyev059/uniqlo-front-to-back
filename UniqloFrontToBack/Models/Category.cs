7c720848bba51fbb8fb6c87e573f87fbd00efd18
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-12 23:58:05 +0400

    added category model with properties name and products.

---
+++ UniqloFrontToBack/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿using UniqloFrontToBack.Models.Base;
+
+namespace UniqloFrontToBack.Models;
+
+public class Category : BaseEntity
+{
+    public string Name { get; set; }
+    public List<Product> Products { get; set; }
+}
